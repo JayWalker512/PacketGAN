61674e5aa055f62301a8b299b80647f54c0a9b1b
Brandon Foltz <bsfoltzguitar@gmail.com>
2019-03-03 12:14:12 -0500

    Fix some bugs and add more utility functions for the framework

--- feature_extraction.py
+++ feature_extraction.py
@@ -6,12 +6,31 @@ import matplotlib.pyplot as plt
 import numpy as np
 import os.path
 
-def test_fn():
-	print("Testing the ability to import functions.")
-	
 #Load a CSV from the UNSW-NB15 dataset into a Pandas DataFrame
-def load_unsw_nb15_dataset_as_data_frame(filePath = None, featuresPath = None):
-	if filePath is None or featuresPath is None:
-		return None
-		
-	
+def load_unsw_nb15_dataset_as_data_frame(file_path = None, features_path = None):
+    if file_path is None or features_path is None:
+        return None
+        
+    
+    features_df = pd.read_csv(features_path, encoding="latin-1")
+    for i in range(len(features_df.columns.values)):
+        features_df.columns.values[i] = str(features_df.columns.values[i]).strip().lower()
+    
+    #lower case all the types
+    for i in range(len(features_df)):
+        features_df.loc[i, ['type']] = str(features_df['type'][i]).strip().lower()
+        features_df.loc[i, ['name']] = str(features_df['name'][i]).strip().lower()
+
+    packet_data_df = pd.read_csv(file_path, encoding="latin-1", names=features_df['name'], header=None)
+
+    return packet_data_df, features_df
+    
+    
+def test_cases():
+    #check that the dataframe is loaded correctly per some pre-determined values
+    data_path = "UNSW-NB15_1_clean.csv"
+    features_path = "UNSW-NB15_features.csv"
+    packet_df, features_df = load_unsw_nb15_dataset_as_data_frame(data_path, features_path)
+    assert (packet_df is not None), "Couldn't load packet dataset"
+    assert (features_df is not None), "Couldn't load features list"
+
